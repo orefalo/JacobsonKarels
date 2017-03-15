bccdc3f1561c1fd8eb69eab205e8cb2ba826a794
Olivier Refalo <orefalo@yahoo.com>
2017-03-15 14:12:10 -0400

    fixes

--- Jacobson.java
+++ Jacobson.java
@@ -10,48 +10,51 @@ Simple Jacobson Karels algorithm,
 
 public class Jacobson {
 
-	// list of samples used for the simulation
-	private static double samples[] = {600, 600, 600, 600, 600, 600, 600, 100, 100, 100, 100, 100, 100, 100, 100, 100, 600, 600, 600, 600, 600};
-
-	
-	// 100ms is a good start for a typical service call
-	private static double estimatedRTT = 100;
-	private static double deviation = 1;
-	
-	public static void main (String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		double timeoutVal = estimatedRTT + 4 * deviation;
+    // list of samples used for the simulation
+    private static double samples[] = {600, 600, 600, 600, 600, 600, 600, 100, 100, 100, 100, 100, 100, 100, 100, 100, 600, 600, 600, 600, 600};
 
 
-		for(int i=0; i<samples.length;i++)
-		{
-			double sample = samples[i];	
+    // 100ms is a good start for a typical service call
+    private static double estimatedRTT = 100;
+    private static double deviation = 1;
 
-			timeoutVal = getNewTimeoutVal(sample);
-			System.out.println("sample:" + sample + " -> estimateRTT:" + estimatedRTT + "ms maxTimeout:"+timeoutVal+"ms      d:"+deviation );
+    public static void main(String[] args) {
 
-		}
+        Scanner scan = new Scanner(System.in);
+        double timeoutVal = estimatedRTT + 4 * deviation;
 
-		while (true) {
 
-			System.out.print("Next API sample timeout (in ms): ");
-			double sample = scan.nextDouble();
-			
-			timeoutVal = getNewTimeoutVal(sample);
-			System.out.println("sample:" + sample + " -> estimateRTT:" + estimatedRTT + "ms maxTimeout:"+timeoutVal+"ms      d:"+deviation );
-		}
-	}
-	
-	public static double getNewTimeoutVal (double sampleRTT) {
-		double difference = sampleRTT - estimatedRTT;
-		estimatedRTT = estimatedRTT + (difference / 8);
+        for (int i = 0; i < samples.length; i++) {
+            double sample = samples[i];
 
-		// the issue with the foumula below is that is negatively react to abrupt changes
-		// for instance, 600ms for a while, the timeout raises to 500mn, then say the service responds at 100ms, timeout will go up to 700ms before coming down
-//		deviation = deviation + (Math.abs(difference) - deviation)/8;
-		deviation = Math.abs(deviation + (difference - deviation)/8);
+            timeoutVal = getNewTimeoutVal(sample);
+            System.out.println("sample:" + sample + " -> estimateRTT:" + estimatedRTT + "ms maxTimeout:" + timeoutVal + "ms      d:" + deviation);
 
-		return estimatedRTT + 4 * deviation;
-	}
+        }
+
+        while (true) {
+
+            System.out.print("Next API sample timeout (in ms): ");
+            double sample = scan.nextDouble();
+
+            timeoutVal = getNewTimeoutVal(sample);
+            System.out.println("sample:" + sample + " -> estimateRTT:" + estimatedRTT + "ms maxTimeout:" + timeoutVal + "ms      d:" + deviation);
+        }
+    }
+
+    public static double getNewTimeoutVal(double sampleRTT) {
+
+        double difference = sampleRTT - estimatedRTT;
+        estimatedRTT = estimatedRTT + (difference / 8);
+
+        // issue: the the formula below negatively reacts to abrupt changes.
+        // for instance, 600ms for a while, the timeout raises to 500ms,
+        //   then say the service responds at 100ms..
+        // you would expect the timeout to shrink, right? well it doesn't ! it will go up to 700ms before coming down
+        //	deviation = deviation + (Math.abs(difference) - deviation)/8;
+
+        // this formula is mutch better
+        deviation = Math.abs(deviation + (difference - deviation) / 8);
+        return estimatedRTT + 4 * deviation;
+    }
 }
